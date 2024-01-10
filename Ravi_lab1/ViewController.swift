a0be9b4c544b15e9ea2561881c392671840a2c80
Ravikumar <gangiseetyravikumar@gmail.com>
2024-01-10 10:28:26 -0800

    Initial Commit

---
+++ Ravi_lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ravi_lab1
+//
+//  Created by user240052 on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
